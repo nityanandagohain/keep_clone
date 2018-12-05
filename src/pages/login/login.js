9b7fe53460b89f8cf4d63270ffc203d9880ab55c
Bishal Deb <bishaldeb656@gmail.com>
2018-12-05 17:51:56 +0530

    Issue #1: design the login page resolved. (#6)
    * Issue #1: design the login page resolved.

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import fire from '../../config/fire';
+import './login.css'
 import Loader from '../loader/loader';
-
 export default class Login extends Component {
     constructor(props) {
         super(props);
@@ -56,27 +56,33 @@ export default class Login extends Component {
         this.setState({ [e.target.name]: e.target.value })
     }
 
+componentDidMount() {
+    document.body.classList.add("background-white");
+}
+
+componentWillUnmount() {
+    document.body.classList.remove("background-white");
+}
     render() {
         return (
             this.state.loading ?
                 <Loader />
                 :
-                <div style={{ margin: '200px' }}>
-                    <div className="card">
-                        <div className="card-body">
-                            <form id="Login">
-                                <div className="form-group">
-                                    <input value={this.state.email} onChange={this.handleChange} name="email" type="email" className="form-control" id="inputEmail" placeholder="Email Address" />
-                                </div>
-                                <div className="form-group">
-                                    <input value={this.state.password} onChange={this.handleChange} name="password" type="password" className="form-control" id="inputPassword" placeholder="Password" />
-                                </div>
-                                <button onClick={this.login} style={{ margin: '20px' }} type="submit" className="btn btn-primary">Login</button>
-                                <button onClick={this.signup} style={{ margin: '20px' }} type="submit" className="btn btn-primary">Sign Up</button>
-                            </form>
-                        </div>
+                    <div className="col-md-6 container box">
+                    <h1 class="form-head">KEEP CLONE</h1>
+                        <form id="Login">
+                            <div className="form-group">
+                                <input value={this.state.email} onChange={this.handleChange} name="email" type="email" className="form-control" id="inputEmail" placeholder="Email Address" />
+                            </div>
+                            <div className="form-group">
+                                <input value={this.state.password} onChange={this.handleChange} name="password" type="password" className="form-control" id="inputPassword" placeholder="Password" />
+                            </div>
+                            <button onClick={this.login} type="submit" className="btn btn-secondary " >Login</button>
+                            <button onClick={this.signup} type="submit" className="btn btn-primary ">Sign Up</button>
+                        </form>
                     </div>
-                </div>
         );
     }
+
+
 }

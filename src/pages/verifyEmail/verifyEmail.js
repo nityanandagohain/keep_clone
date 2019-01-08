3e97e60ab2fcf756f073dbe9016ef2ecc5692d59
Bishal Deb <bishaldeb656@gmail.com>
2019-01-08 13:25:09 +0530

    Bugs in Verify Email page. #50 (#51)
    * Bugs in Verify Email page resolved. #50

--- src/pages/verifyEmail/verifyEmail.js
+++ src/pages/verifyEmail/verifyEmail.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
+import Login from '../login/login.js';
 
 export default class VerifyEmail extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          verify: false,
+        };
+        this.verified = this.verified.bind(this);
+      }
+    verified(){
+        this.setState({
+            verify: !this.state.verify,
+        });
+    }
     render()
     {
         return(
+            <div>
+                {this.state.verify?
+                <Login/>
+            :
             <div className="loader">
                 <div style={{margin:'300px',fontSize:"40px"}}>
                     <p>Please Verify your email to continue using this app(Reload the app if email already verified).</p>
+                    <button className="btn btn-danger" style={{position:"fixed", top:"0",right:"0"}} onClick={this.verified}>Go back to login page</button>
                 </div>
+            </div>
+            }
             </div>
         );
     }

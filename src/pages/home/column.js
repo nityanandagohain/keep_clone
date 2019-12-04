59d07b8105fe181a94521739563a75a98c896a6f
Sankhadip Samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2019-12-04 09:00:48 +0530

    delete option corrected

--- src/pages/home/column.js
+++ src/pages/home/column.js
@@ -2,13 +2,38 @@ import React, { Component } from 'react';
 import Note from '../note/note';
 import './home.css';
 import { Droppable } from 'react-beautiful-dnd';
-
+import fire from '../../config/fire'
 
 export default class Column extends Component {
-    
+
+    removeNote=(id)=>
+    {
+        this.props.removeNote(id);
+        // fire.database().ref(`${this.props.uid}/notes/${id}`).remove()
+        // .then((res)=>
+        // {
+        //     console.log(res);
+        // })
+        // .catch((err)=>
+        // {
+        //     console.log(err);
+        // })
+        // console.log(fire.auth());
+        // console.log(id);
+        // fire.database().ref(this.props.uid).child('notes').remove()
+        // this.db.ref(`notes/${id}`).remove()
+        // .then((res)=>
+        // {
+        //     console.log(res);
+        // })
+        // .catch((err)=>
+        // {
+        //     console.log(err);
+        // })
+    }
     render() {
         return (
-            <div class="notesContainer" style={{width: 400, margin: 'auto'}}>
+            <div className="notesContainer" style={{width: 400, margin: 'auto'}}>
                 <header>
                     <h2>{this.props.column.title}</h2>
                 </header>
@@ -19,14 +44,15 @@ export default class Column extends Component {
                         {...provided.droppableProps}
                         >
                             {this.props.notes.map((note, index) => {
-                                return <Note 
+                                console.log(note);
+                                return note ?  <Note 
                                     key={note.id} 
                                     noteList={note.noteList} 
                                     noteTitle={note.noteTitle} 
                                     noteData={note.noteData} 
                                     noteId={note.id} 
                                     removeNote={this.removeNote} 
-                                    index={index} /> 
+                                    index={index} /> : null
                             })}
                             {provided.placeholder} 
                         </div>
